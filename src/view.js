629ad4d56e8e65808a981c9ca7ba2fcbf504c9f5
Colin Scally <colin.scally@gmail.com>
2017-06-20 16:03:58 +0100

    functionality to display list of notes, and click on an individual note to display it in full

--- src/view.js
+++ src/view.js
@@ -1,8 +1,14 @@
 var writeList = function() {
     var notesList = document.getElementById('noteslist')
-    var note = document.createElement('li');
+    var li = document.createElement('li');
+    var note = document.createElement('BUTTON');
+    var long = pageList.notes[pageList.notes.length -1].content
+    console.log(long)
     note.innerHTML = pageList.list()[pageList.list().length -1];
-    notesList.appendChild(note);
-
-
+    note.addEventListener('click', function(){
+      var noteContent = document.getElementById('note-content')
+      noteContent.innerHTML = long;
+    })
+    li.appendChild(note)
+    notesList.appendChild(li);
 }
