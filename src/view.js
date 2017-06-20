8f944f6e2a7566c86c2e8725ce3214ab501d4450
Simon Ashbery <si.ashbery@gmail.com>
2017-06-20 16:46:12 +0100

    Adds style sheet for interface.

--- src/view.js
+++ src/view.js
@@ -1,5 +1,7 @@
+var pageList = new NoteList();
+
 var writeList = function() {
-    var notesList = document.getElementById('noteslist')
+    var notesList = document.getElementById('notes-list')
     var note = document.createElement('li');
     note.innerHTML = pageList.list()[pageList.list().length -1];
     notesList.appendChild(note);
