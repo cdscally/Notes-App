8726317e8235cc4bdc1c951143f2090255685169
Simon Ashbery <si.ashbery@gmail.com>
2017-06-20 11:45:14 +0100

    List returns abbreviations of each note -Refactors tests to use .notes rather than getList()

--- src/NoteList.js
+++ src/NoteList.js
@@ -3,7 +3,11 @@ function NoteList() {
 }
 
 NoteList.prototype.list = function() {
-  return this.notes;
+  var abbreviatedNotes = [];
+  for(i = 0; i < this.notes.length; i++) {
+    abbreviatedNotes.push(this.abbreviateNote(this.notes[i].content))
+  };
+  return abbreviatedNotes;
 };
 
 NoteList.prototype.createNote = function(content) {
